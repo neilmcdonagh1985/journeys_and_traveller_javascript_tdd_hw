3a62c25dd273f0762aa0dcdda901ff2e1d84e6d6
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-04 19:07:52 +0100

    test commit

--- models/traveller.js
+++ models/traveller.js
@@ -29,6 +29,9 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance
+  }, 0)
 
 };
 
